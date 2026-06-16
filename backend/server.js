1d5aade54562242cc1c05cb0b334482d95844219
imrankhanchauhan36-cmd <imrankhanchauhan36@gmail.com>
2026-06-16 21:36:43 +0530

    fix: use PORT env variable in server.js

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ app.get("/", (req, res) => {
 });
 
 /* ===================== SERVER ===================== */
-const PORT = 5000;
+const PORT = process.env.PORT || 6060;
 app.listen(PORT, () => {
   console.log(`✅ Server running on http://localhost:${PORT}`);
 });
