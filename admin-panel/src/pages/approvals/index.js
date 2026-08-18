48e9bd677e7c33f32bc4b09dc21bd2ed15ff689c
imrankhanchauhan36-cmd <imrankhanchauhan36@gmail.com>
2026-08-19 03:26:38 +0530

    Move Admin Panel to separate repository

--- admin-panel/src/pages/approvals/index.js
+++
@@ -1,8 +0,0 @@
-//////////////////////////////////////////////////////
-// ZEMISH — Approvals Module — Barrel Export
-// App.jsx mein sirf ye import karo:
-// import { ApprovalsPage, ApprovalDetailPage } from './pages/approvals'
-//////////////////////////////////////////////////////
-
-export { default as ApprovalDetailPage } from './ApprovalDetailPage'
-export { default as ApprovalsPage } from './ApprovalsPage'
